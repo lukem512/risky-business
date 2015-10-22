e0419acbdda7b23442b3b14230d8a647af1de7fd
Luke <lukem95@gmail.com>
2015-10-22 18:27:57 +0100

    progress(?) on bubblesort

--- benchmarks/bubble.s
+++ benchmarks/bubble.s
@@ -1,5 +1,6 @@
 % A more complex benchmark
 % This application sorts an array of numbers using Bubble Sort
+% The Bubble Sort algorithm is the naive one, with no optimisations
 %
 %%%%%%%%%%%%%%%%%%%%%%
 % Address of A (A)
@@ -45,57 +46,68 @@ B -8
 %%%%%%%%%%%%%%%%%%%%%%
 %
 %%%%%%%%%%%%%%%%%%%%%%
+%
+% procedure bubbleSort( A : list of sortable items )
+%   n = length(A)
+%   repeat 
+%     swapped = false
+%     for i = 1 to n-1 inclusive do
+%       /* if this pair is out of order */
+%       if A[i-1] > A[i] then
+%         /* swap them and remember something changed */
+%         swap( A[i-1], A[i] )
+%         swapped = true
+%       end if
+%     end for
+%   until not swapped
+% end procedure
+%
+%
 % swapped = false
-LDC r2 0
-% false
-LDC r15 0
+LDC r3 0
 % i = 1
-LDC r3 1
-% incr = 1
 LDC r4 1
-%%%%%%%%%%%%%%%%%%%%%%
-%
-%%%%%%%%%%%%%%%%%%%%%%
-% while (i < len)
-CMP r5 r3 r1
-BLTZ r5 1
-B 14
-%
-% r10 = i-1
-LDC r14 1
-SUB r10 r3 r14
-% r8 = A[i-1]
-ADD r12 r0 r10
-LD r8 r12
-% r9 = A[i]
-ADD r13 r0 r3
-LD r9 r13
-%%%%%%%%%%%%%%%%%%%%%%
-%
-%%%%%%%%%%%%%%%%%%%%%%
-% if(A[i-1] < A[i])
-CMP r11 r8 r9
-BLTZ r11 1
-B 3
-%
-% store A[i] in A[i-1]
-STR r12 A[i]
-% store A[i-1] in A[i]
-STR r13 A[i-1]
+% incr = 1
+LDC r5 1
+% while (i < len) {
+CMP r6 r4 r1
+BLTZ r6 1
+B 15
+% if (A[i-1] > A[i]) {
+% Address of A[i-1]
+SUB r7 r4 r5
+ADD r8 r0 r7
+% Address of A[i]
+ADD r9 r0 r4
+% A[i-1]
+LD r10 r8
+% A[i]
+LD r11 r9
+% if (A[i] < A[i-1]) {
+CMP r12 r11 r10
+BLTZ r12 1
+B 4
+% SWAP THEM
+STR r8 r11
+STR r9 r10
 % swapped = true
-LDC r2 1
-%
+LDC r3 1
 % i = i + incr
-ADD r3 r3 r4
+ADD r4 r4 r5
+PRNT r4
+B -9
+% }
+% }
+B -18
+% }
+% r13 = false
+LDC r13 0
+% if (swapped == false)
+CMP r14 r3 r13
+BZ r14 1
+% else jump to "swapped = false"
+B -28
 %
-B -17
-%%%%%%%%%%%%%%%%%%%%%%
-%
-%%%%%%%%%%%%%%%%%%%%%%
-% Repeat loop if swapped == false
-CMP r5 r2 r15
-% Jump to "LDC r2 0"
-BZ r5 -23
 %%%%%%%%%%%%%%%%%%%%%%
 %
 %%%%%%%%%%%%%%%%%%%%%%
@@ -115,4 +127,5 @@ ADD r3 r3 r4
 B -8
 %%%%%%%%%%%%%%%%%%%%%%
 %
+%%%%%%%%%%%%%%%%%%%%%%
 HLT
